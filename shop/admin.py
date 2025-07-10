85f8eae9763e84339c56938e52bba32ce6ee5bd4
imfgb <fglzb00@gmail.com>
2025-07-10 16:21:05 -0600

    Admin Panel Customisation

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Products,Order
+
 # Register your models here.
 
-admin.site.register(Products)
+admin.site.site_header = "E-commerce"
+admin.site.site_title = "Luxury Watches Ecommerce"
+admin.site.index_title = "Manage Watches"
+
+class ProductAdmin(admin.ModelAdmin):
+    
+    def change_category_to_default(self,request,queryset):
+        queryset.update(category="default")
+    
+    change_category_to_default.short_description = 'Default Category'
+    
+    list_display = ('title','price','discount_price','category','description',)
+    search_fields = ('description',)
+    actions = ('change_category_to_default',)
+    fields = ('title','price','category',)
+    list_editable = ('price','category',)
+    
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
